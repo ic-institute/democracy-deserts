eb69c0c5ccfca94bae8c3bd44f62300f9e1e60ad
Coyote Codornices Marin <coyote@coyote.rocks>
2021-10-04 18:07:06 -0700

    added race/ethnicity data to table. used it to output states table

--- ddeserts/load.py
+++ ddeserts/load.py
@@ -1,5 +1,6 @@
 """Load and parse CSV data"""
 from csv import DictReader
+from itertools import groupby
 
 from pandas import DataFrame
 
@@ -7,6 +8,19 @@ from os.path import basename
 from os.path import join
 
 from .parse import parse_cvap_row
+
+
+# translate single-race lntitle values to three-letter prefixes, and
+# "Total" to no prefix
+LN_PREFIXES = {
+    'Total': '',
+    'American Indian or Alaska Native Alone': 'ind_',
+    'Asian Alone': 'asn_',
+    'Black or African American Alone': 'blk_',
+    'Native Hawaiian or Other Pacific Islander Alone': 'pac_',
+    'White Alone': 'wht_',
+    'Hispanic or Latino': 'his_',
+}
 
 
 CVAP_DATA_DIR = 'data/census/CVAP_2015-2019_ACS_csv_files'
@@ -21,9 +35,11 @@ def load_charter_cities():
 def load_cvap_data(name, *, pre_filter=None, filter=None):
     path = join(CVAP_DATA_DIR, name + '.csv')
 
-    rows = list(read_cvap_csv(path, pre_filter=pre_filter, filter=filter))
+    rows = read_cvap_csv(path, pre_filter=pre_filter, filter=filter)
 
-    df = DataFrame.from_records(rows, index=['table', 'line'])
+    records = rows_to_records(rows)
+
+    df = DataFrame.from_records(records, index=['table', 'line'])
 
     # fix data types
     df['tot_moe'] = df['tot_moe'].astype('int')
@@ -32,6 +48,33 @@ def load_cvap_data(name, *, pre_filter=None, filter=None):
     df['cit_moe'] = df['cit_moe'].astype('int')
 
     return df
+
+
+def rows_to_records(csv_rows):
+    """Turn groups of rows for the same geography into a single record."""
+    for _, rows in groupby(csv_rows, lambda r: r['geoid']):
+        result = None
+
+        for row in rows:
+            if result is None:
+                # values that are the same for all rows in the group
+                result = {
+                    k: v for k, v in row.items()
+                    if k in ('geoid', 'geoname', 'line', 'table')
+                }
+
+            # look up the prefix for whichever ethnic/racial group this
+            # row is about (or '' for the Total row)
+            prefix = LN_PREFIXES.get(row['lntitle'])
+            if prefix is None:
+                continue
+
+            # e.g. if prefix is 'blk_', transform 'tot_est' to 'blk_tot_est'
+            for k, v in row.items():
+                if '_' in k:
+                    result[prefix + k] = int(v)  # values are always numbers
+
+        yield result
 
 
 def read_cvap_csv(path, *, pre_filter=None, filter=None):

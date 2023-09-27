29d41c4f013dbf4ee007803cd047dc9deedb1e6e
Coyote Codornices Marin (they/them) <coyote@coyote.rocks>
2023-09-27 16:01:42 -0700

    slight refactor

--- ddeserts/load.py
+++ ddeserts/load.py
@@ -8,11 +8,9 @@ from os.path import basename
 from os.path import join
 
 from .annotate import LN_PREFIXES
-from .annotate import age_sex_cit_to_cvap
+from .annotate import moe_of_sum
 from .parse import parse_age_sex_cit_row
 from .parse import parse_cvap_row
-
-
 
 
 CVAP_DATA_DIR = 'data/census/CVAP_2017-2021_ACS_csv_files'
@@ -28,10 +26,11 @@ def load_age_sex_cit_data():
     Return it as a stream of rows
     """
     rows = read_age_sex_cit_csv(AGE_SEX_CIT_DATA_PATH)
+    cvap_rows = [age_sex_cit_row_to_cvap(row) for row in rows]
 
-    cvap_rows = [age_sex_cit_to_cvap(row) for row in rows]
+    df = DataFrame.from_records(cvap_rows)
 
-    return cvap_rows
+    return df
 
 
 def load_charter_cities():
@@ -84,8 +83,6 @@ def rows_to_records(csv_rows):
         yield result
 
 
-
-
 def read_age_sex_cit_csv(path):
 
     with open(path, newline='', encoding='latin-1') as f:
@@ -136,3 +133,81 @@ def read_cvap_csv(path, *, pre_filter=None):
             row['table'] = table
 
             yield parsed_row
+
+
+def age_sex_cit_row_to_cvap(row):
+    """Convert a row from the B05033 (Age, Sex, Nativity, and Citizenship)
+    data into data matching the CVAP table. Should contain the following
+    fields:
+
+    line
+    geoname
+    geotype (always 'state')
+    tot_est
+    tot_moe
+    adu_est
+    adu_moe
+    cit_est
+    cit_moe
+    cvap_est
+    cvap_moe
+    """
+    geoname = ''
+    geotype = 'state'
+    tot_est = 0
+    tot_moe = 0
+    adu_ests = []
+    adu_moes = []
+    cit_ests = []
+    cit_moes = []
+    cvap_ests = []
+    cvap_moes = []
+
+    for k, v in row.items():
+        if k == 'Geographic Area Name':
+            geoname = v
+        elif '!!' in k:
+            parts = k.split('!!')
+            parts += [''] * (6 - len(parts))
+
+            data_type, _, sex, age, born, cit = parts
+
+            if not sex:
+                # top level totals
+                if data_type == 'Estimate':
+                    tot_est = int(v)
+                elif data_type == 'Margin of Error':
+                    tot_moe == int(v)
+
+            elif age == '18 years and over:' and not born:
+                # totals for age range, split by sex
+                if data_type == 'Estimate':
+                    adu_ests.append(v)
+                elif data_type == 'Margin of Error':
+                    adu_moes.append(v)
+
+            elif born == 'Native' or cit == 'Naturalized U.S. citizen':
+                # citizen data
+                if data_type == 'Estimate':
+                    cit_ests.append(v)
+                elif data_type == 'Margin of Error':
+                    cit_moes.append(v)
+
+                if age == '18 years and over:':
+                    if data_type == 'Estimate':
+                        cvap_ests.append(v)
+                    elif data_type == 'Margin of Error':
+                        cvap_moes.append(v)
+
+    return dict(
+        geoname=geoname,
+        geotype=geotype,
+        tot_est=tot_est,
+        tot_moe=tot_moe,
+        adu_est=sum(adu_ests),
+        adu_moe=moe_of_sum(*adu_moes),
+        cit_est=sum(cit_ests),
+        cit_moe=moe_of_sum(*cit_moes),
+        cvap_est=sum(cvap_ests),
+        cvap_moe=moe_of_sum(*cvap_moes),
+    )
